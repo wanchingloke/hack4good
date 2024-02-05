f733d483f67968fbf53614192d51f337b6b14e71
curtischang2510 <curtischang2510@gmail.com>
2024-02-05 23:37:46 +0800

    Setting up login page

--- client/src/Login.js
+++ client/src/Login.js
@@ -1,11 +1,18 @@
 import React from "react"
-
+import React, {useState} from 'react' 
+import axios from 'axios'
 function Login(){
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    function handleSubmit(event){
+        event.preventDefault();
+        axios.post('')
+    }
     return (
         <div>
             <div>
                 <form>
-                    <div>
+                    <div onSubmit={handleSubmit}>
                         <label htmlFor="roleSelect">You are a: </label>
                         <select id="roleSelect">
                             <option value="">Select...</option>
@@ -15,11 +22,13 @@ function Login(){
                     </div>
                     <div>
                         <label htmlFor="email">Email</label>
-                        <input type="email" placeholder="Enter Email"/>
+                        <input type="email" placeholder="Enter Email"
+                        onChange={e => setEmail(e.target.value)}/>
                     </div>
                     <div>
                         <label htmlFor="password">Password</label>
-                        <input type="password" placeholder="Enter Password"/>
+                        <input type="password" placeholder="Enter Password" 
+                        onChange={e => setPassword(e.target.value)}/>
                     </div>
                     <button>Login</button>
                 </form>
