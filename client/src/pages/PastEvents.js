770c8b381faa6b4d9e6e899935f02ba4ef0a8c34
wanchingloke <wanchingloke@gmail.com>
2024-02-08 22:43:29 +0800

    Finished Past Events Page

--- client/src/pages/PastEvents.js
+++ client/src/pages/PastEvents.js
@@ -1,9 +1,9 @@
 
 export default function PastEvents(props) {
   return (
-    <div className="items-start px-5 pt-12">
-      <div className="flex flex-col items-center self-stretch pl-20 mt-40 w-full max-md:pl-5 max-md:mt-10 max-md:max-w-full">
-        <div className="self-stretch ml-2.5 max-md:max-w-full">
+    <div className="items-start px-5">
+      <div className="flex flex-col items-center self-stretch pl-20 mt-5 w-full max-md:pl-5 max-md:mt-10 max-md:max-w-full">
+        <div className="self-stretch max-md:max-w-full">
           <div className="flex gap-5 max-md:flex-col max-md:gap-0 max-md:">
             <div className="flex flex-col w-[33%] max-md:ml-0 max-md:w-full">
               <img
@@ -51,62 +51,62 @@ export default function PastEvents(props) {
           Scroll to read more!
         </div>
       </div>
-      <div className="flex overflow-hidden relative flex-col justify-center items-end py-7 pl-16 mt-20 max-w-full text-4xl font-light text-black min-h-[826px] w-[1020px] max-md:pl-5 max-md:mt-10">
-        <img
-          loading="lazy"
-          srcSet="https://cdn.builder.io/api/v1/image/assets/TEMP/75ea03be6cb18ae343d116c20667a6da9abdfb720ac1203f632c7a45559f4afb?apiKey=0cba06582df446a9859eb90c47a74d7f&width=100 100w, https://cdn.builder.io/api/v1/image/assets/TEMP/75ea03be6cb18ae343d116c20667a6da9abdfb720ac1203f632c7a45559f4afb?apiKey=0cba06582df446a9859eb90c47a74d7f&width=200 200w, https://cdn.builder.io/api/v1/image/assets/TEMP/75ea03be6cb18ae343d116c20667a6da9abdfb720ac1203f632c7a45559f4afb?apiKey=0cba06582df446a9859eb90c47a74d7f&width=400 400w, https://cdn.builder.io/api/v1/image/assets/TEMP/75ea03be6cb18ae343d116c20667a6da9abdfb720ac1203f632c7a45559f4afb?apiKey=0cba06582df446a9859eb90c47a74d7f&width=800 800w, https://cdn.builder.io/api/v1/image/assets/TEMP/75ea03be6cb18ae343d116c20667a6da9abdfb720ac1203f632c7a45559f4afb?apiKey=0cba06582df446a9859eb90c47a74d7f&width=1200 1200w, https://cdn.builder.io/api/v1/image/assets/TEMP/75ea03be6cb18ae343d116c20667a6da9abdfb720ac1203f632c7a45559f4afb?apiKey=0cba06582df446a9859eb90c47a74d7f&width=1600 1600w, https://cdn.builder.io/api/v1/image/assets/TEMP/75ea03be6cb18ae343d116c20667a6da9abdfb720ac1203f632c7a45559f4afb?apiKey=0cba06582df446a9859eb90c47a74d7f&width=2000 2000w, https://cdn.builder.io/api/v1/image/assets/TEMP/75ea03be6cb18ae343d116c20667a6da9abdfb720ac1203f632c7a45559f4afb?apiKey=0cba06582df446a9859eb90c47a74d7f&"
-          className="object-cover absolute inset-0 size-full"
-        />
-        <div className="relative justify-center items-end pt-16 pr-24 pb-36 pl-16 bg-rose-50 max-md:pr-8 max-md:pb-10 max-md:pl-5 max-md:max-w-full">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-        </div>
+      <div className="flex pt-10">
+          <img
+            loading="lazy"
+            srcSet="https://cdn.builder.io/api/v1/image/assets/TEMP/75ea03be6cb18ae343d116c20667a6da9abdfb720ac1203f632c7a45559f4afb?apiKey=0cba06582df446a9859eb90c47a74d7f&width=100 100w, https://cdn.builder.io/api/v1/image/assets/TEMP/75ea03be6cb18ae343d116c20667a6da9abdfb720ac1203f632c7a45559f4afb?apiKey=0cba06582df446a9859eb90c47a74d7f&width=200 200w, https://cdn.builder.io/api/v1/image/assets/TEMP/75ea03be6cb18ae343d116c20667a6da9abdfb720ac1203f632c7a45559f4afb?apiKey=0cba06582df446a9859eb90c47a74d7f&width=400 400w, https://cdn.builder.io/api/v1/image/assets/TEMP/75ea03be6cb18ae343d116c20667a6da9abdfb720ac1203f632c7a45559f4afb?apiKey=0cba06582df446a9859eb90c47a74d7f&width=800 800w, https://cdn.builder.io/api/v1/image/assets/TEMP/75ea03be6cb18ae343d116c20667a6da9abdfb720ac1203f632c7a45559f4afb?apiKey=0cba06582df446a9859eb90c47a74d7f&width=1200 1200w, https://cdn.builder.io/api/v1/image/assets/TEMP/75ea03be6cb18ae343d116c20667a6da9abdfb720ac1203f632c7a45559f4afb?apiKey=0cba06582df446a9859eb90c47a74d7f&width=1600 1600w, https://cdn.builder.io/api/v1/image/assets/TEMP/75ea03be6cb18ae343d116c20667a6da9abdfb720ac1203f632c7a45559f4afb?apiKey=0cba06582df446a9859eb90c47a74d7f&width=2000 2000w, https://cdn.builder.io/api/v1/image/assets/TEMP/75ea03be6cb18ae343d116c20667a6da9abdfb720ac1203f632c7a45559f4afb?apiKey=0cba06582df446a9859eb90c47a74d7f&"
+            className="size-1/2 flex-row"
+          />
+          <div className="relative items-end flex-row bg-rose-50 float-right inline p-11 text-2xl justify-center pt-36 leading-relaxed px-16">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+            minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </div>
       </div>
-      <div className="z-10 mt-11 ml-4 text-8xl italic text-black backdrop-blur-[2px] max-md:mt-10 max-md:max-w-full max-md:text-4xl">
+      <div className="z-10 mt-11 ml-4 text-6xl italic backdrop-blur-[2px] max-md:mt-10 max-md:max-w-full max-md:text-4xl">
         Working with children
       </div>
-      <div className="flex overflow-hidden relative flex-col justify-center items-end py-12 pl-16 max-w-full text-4xl font-light text-black min-h-[901px] w-[1055px] max-md:pl-5">
-        <img
-          loading="lazy"
-          srcSet="https://cdn.builder.io/api/v1/image/assets/TEMP/11de4edf0d95fb055b134b680f64ea2407769ce3ab3fde4a80254fb61b45818d?apiKey=0cba06582df446a9859eb90c47a74d7f&width=100 100w, https://cdn.builder.io/api/v1/image/assets/TEMP/11de4edf0d95fb055b134b680f64ea2407769ce3ab3fde4a80254fb61b45818d?apiKey=0cba06582df446a9859eb90c47a74d7f&width=200 200w, https://cdn.builder.io/api/v1/image/assets/TEMP/11de4edf0d95fb055b134b680f64ea2407769ce3ab3fde4a80254fb61b45818d?apiKey=0cba06582df446a9859eb90c47a74d7f&width=400 400w, https://cdn.builder.io/api/v1/image/assets/TEMP/11de4edf0d95fb055b134b680f64ea2407769ce3ab3fde4a80254fb61b45818d?apiKey=0cba06582df446a9859eb90c47a74d7f&width=800 800w, https://cdn.builder.io/api/v1/image/assets/TEMP/11de4edf0d95fb055b134b680f64ea2407769ce3ab3fde4a80254fb61b45818d?apiKey=0cba06582df446a9859eb90c47a74d7f&width=1200 1200w, https://cdn.builder.io/api/v1/image/assets/TEMP/11de4edf0d95fb055b134b680f64ea2407769ce3ab3fde4a80254fb61b45818d?apiKey=0cba06582df446a9859eb90c47a74d7f&width=1600 1600w, https://cdn.builder.io/api/v1/image/assets/TEMP/11de4edf0d95fb055b134b680f64ea2407769ce3ab3fde4a80254fb61b45818d?apiKey=0cba06582df446a9859eb90c47a74d7f&width=2000 2000w, https://cdn.builder.io/api/v1/image/assets/TEMP/11de4edf0d95fb055b134b680f64ea2407769ce3ab3fde4a80254fb61b45818d?apiKey=0cba06582df446a9859eb90c47a74d7f&"
-          className="object-cover absolute inset-0 size-full"
-        />
-        <div className="relative justify-center items-end px-16 pt-24 pb-28 mt-8 bg-rose-50 max-md:py-10 max-md:pr-7 max-md:pl-5 max-md:max-w-full">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-        </div>
+      <div className="flex pt-10">
+          <img
+            loading="lazy"
+            srcSet="https://cdn.builder.io/api/v1/image/assets/TEMP/11de4edf0d95fb055b134b680f64ea2407769ce3ab3fde4a80254fb61b45818d?apiKey=0cba06582df446a9859eb90c47a74d7f&width=100 100w, https://cdn.builder.io/api/v1/image/assets/TEMP/11de4edf0d95fb055b134b680f64ea2407769ce3ab3fde4a80254fb61b45818d?apiKey=0cba06582df446a9859eb90c47a74d7f&width=200 200w, https://cdn.builder.io/api/v1/image/assets/TEMP/11de4edf0d95fb055b134b680f64ea2407769ce3ab3fde4a80254fb61b45818d?apiKey=0cba06582df446a9859eb90c47a74d7f&width=400 400w, https://cdn.builder.io/api/v1/image/assets/TEMP/11de4edf0d95fb055b134b680f64ea2407769ce3ab3fde4a80254fb61b45818d?apiKey=0cba06582df446a9859eb90c47a74d7f&width=800 800w, https://cdn.builder.io/api/v1/image/assets/TEMP/11de4edf0d95fb055b134b680f64ea2407769ce3ab3fde4a80254fb61b45818d?apiKey=0cba06582df446a9859eb90c47a74d7f&width=1200 1200w, https://cdn.builder.io/api/v1/image/assets/TEMP/11de4edf0d95fb055b134b680f64ea2407769ce3ab3fde4a80254fb61b45818d?apiKey=0cba06582df446a9859eb90c47a74d7f&width=1600 1600w, https://cdn.builder.io/api/v1/image/assets/TEMP/11de4edf0d95fb055b134b680f64ea2407769ce3ab3fde4a80254fb61b45818d?apiKey=0cba06582df446a9859eb90c47a74d7f&width=2000 2000w, https://cdn.builder.io/api/v1/image/assets/TEMP/11de4edf0d95fb055b134b680f64ea2407769ce3ab3fde4a80254fb61b45818d?apiKey=0cba06582df446a9859eb90c47a74d7f&"
+            className="size-1/2 flex-row"
+          />
+          <div className="relative items-end flex-row bg-rose-50 float-right inline p-11 text-2xl justify-center pt-36 leading-relaxed px-16">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+            minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </div>
       </div>
-      <div className="z-10 mt-4 text-8xl text-black backdrop-blur-[2px] max-md:max-w-full max-md:text-4xl">
+      <div className="z-10 mt-11 ml-4 text-6xl italic backdrop-blur-[2px] max-md:mt-10 max-md:max-w-full max-md:text-4xl">
         Working with the elderly
       </div>
-      <div className="flex overflow-hidden relative flex-col items-end pt-12 pb-4 pl-16 -mt-10 max-w-full text-4xl font-light text-black min-h-[888px] w-[1020px] max-md:pl-5">
-        <img
-          loading="lazy"
-          srcSet="https://cdn.builder.io/api/v1/image/assets/TEMP/30149a35ca2c9f2bd49fd3e5538fcb13a2923ab98568bbc2c0242c3db275b510?apiKey=0cba06582df446a9859eb90c47a74d7f&width=100 100w, https://cdn.builder.io/api/v1/image/assets/TEMP/30149a35ca2c9f2bd49fd3e5538fcb13a2923ab98568bbc2c0242c3db275b510?apiKey=0cba06582df446a9859eb90c47a74d7f&width=200 200w, https://cdn.builder.io/api/v1/image/assets/TEMP/30149a35ca2c9f2bd49fd3e5538fcb13a2923ab98568bbc2c0242c3db275b510?apiKey=0cba06582df446a9859eb90c47a74d7f&width=400 400w, https://cdn.builder.io/api/v1/image/assets/TEMP/30149a35ca2c9f2bd49fd3e5538fcb13a2923ab98568bbc2c0242c3db275b510?apiKey=0cba06582df446a9859eb90c47a74d7f&width=800 800w, https://cdn.builder.io/api/v1/image/assets/TEMP/30149a35ca2c9f2bd49fd3e5538fcb13a2923ab98568bbc2c0242c3db275b510?apiKey=0cba06582df446a9859eb90c47a74d7f&width=1200 1200w, https://cdn.builder.io/api/v1/image/assets/TEMP/30149a35ca2c9f2bd49fd3e5538fcb13a2923ab98568bbc2c0242c3db275b510?apiKey=0cba06582df446a9859eb90c47a74d7f&width=1600 1600w, https://cdn.builder.io/api/v1/image/assets/TEMP/30149a35ca2c9f2bd49fd3e5538fcb13a2923ab98568bbc2c0242c3db275b510?apiKey=0cba06582df446a9859eb90c47a74d7f&width=2000 2000w, https://cdn.builder.io/api/v1/image/assets/TEMP/30149a35ca2c9f2bd49fd3e5538fcb13a2923ab98568bbc2c0242c3db275b510?apiKey=0cba06582df446a9859eb90c47a74d7f&"
-          className="object-cover absolute inset-0 size-full"
-        />
-        <div className="relative justify-center items-end pt-28 pr-20 pb-24 pl-16 mt-12 bg-rose-50 max-md:py-10 max-md:pr-8 max-md:pl-5 max-md:mt-10 max-md:max-w-full">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-        </div>
+      <div className="flex pt-10">
+          <img
+            loading="lazy"
+            srcSet="https://cdn.builder.io/api/v1/image/assets/TEMP/30149a35ca2c9f2bd49fd3e5538fcb13a2923ab98568bbc2c0242c3db275b510?apiKey=0cba06582df446a9859eb90c47a74d7f&width=100 100w, https://cdn.builder.io/api/v1/image/assets/TEMP/30149a35ca2c9f2bd49fd3e5538fcb13a2923ab98568bbc2c0242c3db275b510?apiKey=0cba06582df446a9859eb90c47a74d7f&width=200 200w, https://cdn.builder.io/api/v1/image/assets/TEMP/30149a35ca2c9f2bd49fd3e5538fcb13a2923ab98568bbc2c0242c3db275b510?apiKey=0cba06582df446a9859eb90c47a74d7f&width=400 400w, https://cdn.builder.io/api/v1/image/assets/TEMP/30149a35ca2c9f2bd49fd3e5538fcb13a2923ab98568bbc2c0242c3db275b510?apiKey=0cba06582df446a9859eb90c47a74d7f&width=800 800w, https://cdn.builder.io/api/v1/image/assets/TEMP/30149a35ca2c9f2bd49fd3e5538fcb13a2923ab98568bbc2c0242c3db275b510?apiKey=0cba06582df446a9859eb90c47a74d7f&width=1200 1200w, https://cdn.builder.io/api/v1/image/assets/TEMP/30149a35ca2c9f2bd49fd3e5538fcb13a2923ab98568bbc2c0242c3db275b510?apiKey=0cba06582df446a9859eb90c47a74d7f&width=1600 1600w, https://cdn.builder.io/api/v1/image/assets/TEMP/30149a35ca2c9f2bd49fd3e5538fcb13a2923ab98568bbc2c0242c3db275b510?apiKey=0cba06582df446a9859eb90c47a74d7f&width=2000 2000w, https://cdn.builder.io/api/v1/image/assets/TEMP/30149a35ca2c9f2bd49fd3e5538fcb13a2923ab98568bbc2c0242c3db275b510?apiKey=0cba06582df446a9859eb90c47a74d7f&"
+            className="size-1/2 flex-row"
+          />         
+          <div className="relative items-end flex-row bg-rose-50 float-right inline p-11 text-2xl justify-center pt-36 leading-relaxed px-16">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+            minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </div>
       </div>
-      <div className="mt-6 ml-4 text-8xl italic text-black backdrop-blur-[2px] max-md:max-w-full max-md:text-4xl">
-        Fitness-based Volunteering
+      <div className="z-10 mt-11 ml-4 text-6xl italic backdrop-blur-[2px] max-md:mt-10 max-md:max-w-full max-md:text-4xl">
+        Fitness-based volunteering
       </div>
     </div>
   );
